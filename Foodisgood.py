3e9950ac6f609605a1295fd6c85e32d34dcea769
Sousou999 <161655129+Sousou999@users.noreply.github.com>
2024-04-07 23:39:16 +0200

    Add files via upload

--- Foodisgood.py
+++ Foodisgood.py
@@ -15,21 +15,38 @@ breakfast_recipes_data = {
         "Lachs-Sandwich",
         "Acai-Bowl",
         "Apfel-Nuss-Müsli",
-        "Räucherlachs mit Rührei"
+        "Räucherlachs mit Rührei",
+        "Quinoa-Pfannkuchen mit Blaubeeren und Mandeln",
+        "Bircher Müsli mit Nüssen und Honig",
+        "Frittata mit Spargel, Tomaten und Avocado",
+        "Bagel-Sandwich mit Ei, Schinken und Käse",
+        "Frühstücks-Burrito mit Gemüse und Salsa",
+        "Vanille-Chia-Pudding mit frischem Obst",
+        "Bananen-Pancakes mit Beerenkompott",
+        "Rösti mit pochiertem Ei und Gemüse",
+        "Tofu-Scramble mit schwarzen Bohnen und Avocado"
     ],
     "Zutaten": [
-        "Haferflocken, Beeren (z.B. Himbeeren, Blaubeeren), Milch oder Joghurt, Honig",
-        "Avocado, Vollkornbrot, Ei, Salz, Pfeffer",
-        "Naturjoghurt, Müsli, Früchte (z.B. Banane, Erdbeeren)",
-        "Eier, Gemüse (z.B. Paprika, Spinat), Salz, Pfeffer",
-        "Gemischte gefrorene Früchte (z.B. Banane, Beeren), Mandelmilch, Toppings (z.B. Granola, Kokosraspeln)",
-        "Mehl, Eier, Milch, Zucker, Backpulver, Ahornsirup",
-        "Chiasamen, Mandelmilch, Früchte (z.B. Erdbeeren, Mango)",
-        "Räucherlachs, Frischkäse, Gurke, Dill, Vollkornbrot",
-        "Acai-Püree, Banane, Beeren (z.B. Blaubeeren, Erdbeeren), Granola, Kokosraspeln",
-        "Haferflocken, Nüsse, Joghurt, Apfel, Honig",
-        "Räucherlachs, Eier, Vollkornbrot, Salz, Pfeffer"
-    
+        "50g Haferflocken, 100g Beeren (z.B. Himbeeren, Blaubeeren), 200ml Milch oder Joghurt, 1 EL Honig",
+        "1 Avocado, 2 Scheiben Vollkornbrot, 1 Ei, Salz, Pfeffer",
+        "150g Naturjoghurt, 50g Müsli, 100g frische Früchte (z.B. Banane, Erdbeeren), 1 EL Honig",
+        "3 Eier, 100g Gemüse (z.B. Paprika, Spinat), Salz, Pfeffer",
+        "200g gemischte gefrorene Früchte (z.B. Banane, Beeren), 150ml Mandelmilch, Toppings (z.B. Granola, Kokosraspeln)",
+        "100g Mehl, 1 Ei, 150ml Milch, 1 EL Zucker, 1 TL Backpulver, Ahornsirup",
+        "3 EL Chiasamen, 250ml Mandelmilch, 100g Früchte (z.B. Erdbeeren, Mango)",
+        "100g Räucherlachs, 50g Frischkäse, 1/2 Gurke, Dill, 2 Scheiben Vollkornbrot",
+        "100g Acai-Püree, 1 Banane, 50g Beeren (z.B. Blaubeeren, Erdbeeren), 30g Granola, 1 EL Kokosraspeln",
+        "50g Haferflocken, 30g gemischte Nüsse, 150g Joghurt, 1 Apfel, 1 EL Honig",
+        "100g Räucherlachs, 3 Eier, 2 Scheiben Vollkornbrot, Salz, Pfeffer",
+        "100g Quinoa-Mehl, 2 Eier, 150ml Milch (oder Mandelmilch), 50g Blaubeeren, 30g gehackte Mandeln, 1 EL Honig",
+        "50g Haferflocken, 100g Joghurt, 50ml Milch, 1 Apfel, 30g gemischte Nüsse, 1 EL Honig",
+        "4 Eier, 100g grüner Spargel, 50g Kirschtomaten, 1 Avocado, Salz, Pfeffer",
+        "1 Bagel, 2 Eier, 50g Schinken, 50g Käse, Salz, Pfeffer",
+        "2 Tortillas, 100g Gemüse (z.B. Paprika, Zwiebeln), 50g Salsa, 1 Avocado",
+        "3 EL Chiasamen, 250ml Mandelmilch, 1 TL Vanilleextrakt, 100g frisches Obst",
+        "100g Mehl, 1 reife Banane, 2 EL Erdnussbutter, 150ml Milch, 1 EL Honig, 100g gemischte Beeren",
+        "2 große Kartoffeln, 2 Eier, 1 Zwiebel, 1 Paprika, Salz, Pfeffer",
+        "200g Tofu, 100g schwarze Bohnen, 1 Paprika, 1 Zwiebel, 2 Knoblauchzehen, Salz, Pfeffer, 2 Tortillas"
     ],
     "Schwierigkeitsgrad": [
         "Sehr leicht",
@@ -42,7 +59,16 @@ breakfast_recipes_data = {
         "Leicht",
         "Leicht",
         "Sehr leicht",
-        "Leicht"
+        "Leicht",
+        "Mittel",
+        "Mittel",
+        "Mittel",
+        "Mittel",
+        "Mittel",
+        "Mittel",
+        "Mittel",
+        "Schwer",
+        "Schwer"
     ],
     "Dauer": [
         "10 Minuten",
@@ -55,7 +81,16 @@ breakfast_recipes_data = {
         "10 Minuten",
         "5 Minuten",
         "5 Minuten",
-        "10 Minuten"
+        "10 Minuten",
+        "30 Minuten",
+        "10 Minuten",
+        "20 Minuten",
+        "15 Minuten",
+        "15 Minuten",
+        "5 Minuten",
+        "20 Minuten",
+        "20 Minuten",
+        "25 Minuten"
     ],
     "Bild": [
         "https://www.edeka.de/media/01-rezeptbilder/rezeptbilder-i-p/rez-edeka-porridge-mit-beeren-rezept-i-p-1-1.jpg",
@@ -63,25 +98,43 @@ breakfast_recipes_data = {
         "https://kochclub.schuhbeck.de/wp-content/uploads/2017/05/knuspermuesli.jpg",
         "https://www.paramediform.ch/app/uploads/2023/01/Gemueseomelette_gez-scaled-jpg-webp.webp",
         "https://zestysouthindiankitchen.com/wp-content/uploads/2019/02/Berry-smoothie-bowl-2.jpg",
-        "https://www.spendwithpennies.com/wp-content/uploads/2021/01/Banana-Pancakes-SpendWithPennies-1.jpg",
-        "https://www.linsfood.com/wp-content/uploads/2017/07/Quinoa-Breakfast-Bowl-3.jpg",
-        "https://www.simplyrecipes.com/thmb/2Sd8OgRC1N7Yai-7C2WtZBXZoMg=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Simply-Recipes-Smoked-Salmon-and-Avocado-Tartine-LEAD-2-5b834e02ba61770036ec5ae1.jpg",
-        "https://images.immediate.co.uk/production/volatile/sites/30/2021/03/Acai-Bowl-3d0780d.jpg",
-        "https://wholeandheavenlyoven.com/wp-content/uploads/2019/09/Apple-Cinnamon-Granola-9.jpg",
-        "https://www.olivemagazine.com/wp-content/uploads/2017/01/wholewheat-rasps-smoked-salmon-eggs.jpg"
+        "https://www.grillfuerst.de/magazin/wp-content/uploads/2020/03/Pancake-Ahornsirup-scaled.jpg?v=1665135735",
+        "https://www.foodtempel.de/wp-content/uploads/2023/03/Veganer-Chiapudding-mit-Erdbeeren-und-Mandeln.jpg",
+        "https://snackconnection-marktplatz.de/wp-content/uploads/2023/02/Lachs-Gurken-Sandwich-c-123rf-azurita.jpg",
+        "https://www.twopeasandtheirpod.com/wp-content/uploads/2023/06/Acai-Bowl-10.jpg",
+        "https://tribalance.de/cdn/shop/articles/tri.balance_Rezept-Tipp_Muesli.jpg?v=1700628257",
+        "https://rezept-db.womenshealth.de/image/rezept-db/fullWidth/Ulrike-Holsten_RuehreiRaeucherlachs_800x462.jpg.webp",
+        "https://images.eatsmarter.de/sites/default/files/styles/max_size/public/quinoa-pancakes-641304-1.jpg",
+        "https://www.oma-kocht.de/wp-content/uploads/2022/03/bircher-musli.jpg",
+        "https://www.koch-mit.de/app/uploads/2019/05/Gruene_Spargelfrittata.jpeg",
+        "https://media02.stockfood.com/largepreviews/Mzg0ODQwOTc1/12414225-Bagel-belegt-mit-Ei-Kaese-und-Speck.jpg",
+        "https://files.vegan-taste-week.de/1/Burrito-Wrap_Timolina_fotolia-1036x414.jpg",
+        "https://img.over-blog-kiwi.com/1/45/44/32/20150730/ob_b97941_dsc-1800.JPG",
+        "https://klaraslife.com/wp-content/uploads/2017/01/DSC01951.jpg",
+        "https://media01.stockfood.com/largepreviews/NDE4MDI5Mjk2/13484816-Roesti-mit-pochierten-Eiern.jpg",
+        "https://www.lowcarb.de/files/styles/mainimage_normal/public/assets/image/2021/06/17/tofu-scramble_mit_avocado_portrait.jpg?itok=TcQj08iX&t=1632710627"
     ],
     "Anleitung": [
-        "1. Haferflocken mit Milch oder Joghurt in einer Schüssel vermengen. 2. Frische Beeren hinzufügen und mit Honig süßen. 3. Sofort servieren.",
+        "1. Haferflocken in einer Schüssel mit Milch oder Joghurt vermengen. 2. Frische Beeren hinzufügen und mit Honig süßen. 3. Sofort servieren.",
         "1. Avocado halbieren, entkernen und das Fruchtfleisch auf Toastbrot streichen. 2. Ein Ei in einer Pfanne braten und auf die Avocado-Toast legen. 3. Mit Salz und Pfeffer würzen und servieren.",
         "1. Naturjoghurt in eine Schüssel geben. 2. Müsli und frische Früchte hinzufügen. 3. Nach Belieben mit Honig süßen und sofort genießen.",
-        "1. Eier verquirlen und mit Salz und Pfeffer würzen. 2. Gemüse nach Wahl (z.B. Paprika, Spinat) in einer Pfanne anbraten. 3. Eier über das Gemüse gießen und stocken lassen. 4. Omelett vorsichtig falten und servieren.",
+        "1. Eier verquirlen und mit Salz und Pfeffer würzen. 2. Gemüse nach Wahl (z.B. Paprika, Spinat) in einer Pfanne anbraten. 3. Eier darüber gießen und stocken lassen. 4. Omelett vorsichtig falten und servieren.",
         "1. Gefrorene Früchte und Mandelmilch in einen Mixer geben und cremig mixen. 2. In einer Schüssel anrichten und mit Toppings (z.B. Granola, Kokosraspeln) garnieren.",
         "1. Mehl, Eier, Milch, Zucker und Backpulver zu einem Teig verrühren. 2. Pancakes in einer Pfanne goldbraun backen und mit Ahornsirup servieren.",
         "1. Chiasamen und Mandelmilch vermischen und über Nacht im Kühlschrank quellen lassen. 2. Am nächsten Morgen mit Früchten garnieren und genießen.",
         "1. Räucherlachs, Frischkäse, Gurke und Dill auf Vollkornbrot schichten. 2. Mit etwas Zitrone beträufeln und servieren.",
         "1. Acai-Püree in eine Schüssel geben. 2. Mit geschnittenen Bananen und Beeren belegen. 3. Mit Granola und Kokosraspeln toppen und sofort genießen.",
         "1. Haferflocken, Nüsse und Joghurt in eine Schüssel geben. 2. Apfel in Stücke schneiden und dazugeben. 3. Mit Honig süßen und gut durchmischen.",
-        "1. Räucherlachs in eine Pfanne geben und leicht erhitzen. 2. Eier darüber gießen und zu Rührei stocken lassen. 3. Auf Vollkornbrot anrichten und servieren."
+        "1. Räucherlachs in eine Pfanne geben und leicht erhitzen. 2. Eier darüber gießen und zu Rührei stocken lassen. 3. Auf Vollkornbrot anrichten und servieren.",
+        "1. In einer Schüssel das Quinoa-Mehl, Eier und Milch zu einem glatten Teig verrühren. 2. Eine Pfanne leicht einfetten und erhitzen. Den Teig portionsweise in die Pfanne geben und Blaubeeren darauf streuen. 3. Von beiden Seiten goldbraun backen, bis die Pfannkuchen durchgegart sind. 4. Mit gehackten Mandeln bestreuen und mit Honig servieren.",
+        "1. Haferflocken, Joghurt, Milch, Apfelstücke, Nüsse und Honig in einer Schüssel mischen. 2. Über Nacht im Kühlschrank ziehen lassen und am nächsten Morgen mit frischen Früchten und Honig garnieren.",
+        "1. Eier verquirlen und mit Salz und Pfeffer würzen. 2. Spargel, Tomaten und Avocado in Stücke schneiden. 3. Eier darüber gießen und in der Pfanne stocken lassen. 4. Frittata aus der Pfanne nehmen und in Stücke schneiden.",
+        "1. Bagel halbieren und leicht toasten. 2. Eier in einer Pfanne braten und auf den Bagel legen. 3. Schinken und Käse auf das Ei legen und den Bagel zuklappen. 4. Mit Salz und Pfeffer würzen und servieren.",
+        "1. Tortillas leicht erwärmen. 2. Gemüse in einer Pfanne anbraten und auf die Tortillas geben. 3. Salsa und Avocado hinzufügen, Tortillas aufrollen und servieren.",
+        "1. Chiasamen mit Mandelmilch und Vanilleextrakt vermengen. 2. Über Nacht im Kühlschrank quellen lassen. 3. Am nächsten Morgen mit frischem Obst garnieren und servieren.",
+        "1. Mehl, zerdrückte Banane, Erdnussbutter, Milch und Honig zu einem Teig verrühren. 2. Pancakes in einer Pfanne goldbraun backen. 3. Mit Beerenkompott und Honig servieren.",
+        "1. Kartoffeln reiben und abtropfen lassen. 2. Zwiebel fein hacken. 3. Kartoffeln und Zwiebeln in einer Pfanne goldbraun braten. 4. Pochierte Eier zubereiten und auf den Rösti legen. 5. Mit Salz und Pfeffer würzen und servieren.",
+        "1. Tofu zerbröseln und in einer Pfanne anbraten. 2. Schwarze Bohnen, Paprika, Zwiebeln und Knoblauch hinzufügen. 3. Alles gut vermischen und mit Salz und Pfeffer würzen. 4. Auf Tortillas verteilen und mit Avocado servieren."
     ]
 }
 
@@ -91,7 +144,6 @@ breakfast_recipes_df = pd.DataFrame(breakfast_recipes_data)
 # Seitentitel festlegen
 st.title("Willkommen bei FoodisGood")
 st.subheader("Entdecke eine Welt voller gesunder und schmackhafter Frühstücksrezepte für ein energiegeladenes Studium!")
-
 
 # Rezeptsuche nach Frühstücksrezepten
 search_term = st.text_input("Suche nach einem Frühstücksrezept:")
@@ -131,7 +183,10 @@ emoji_surprises = {
     "🍣 Lachs ist reich an Omega-3-Fettsäuren und passt perfekt zu einem herzhaften Frühstück!",
     "🍇 Acai-Bowls sind eine leckere Möglichkeit, viele Vitamine und Antioxidantien zu genießen!",
     "🍏 Äpfel sind gesund, knackig und eine perfekte Ergänzung für jedes Müsli oder Joghurt!",
-    "🍳 Rührei mit Räucherlachs ist ein klassisches, proteinreiches Frühstück für Feinschmecker!"
+    "🍳 Rührei mit Räucherlachs ist ein klassisches, proteinreiches Frühstück für Feinschmecker!",
+    "🥯 Bagels sind eine köstliche Basis für herzhafte Frühstückssandwiches!",
+    "🥔 Rösti ist eine knusprige Kartoffelbeilage und perfekt für ein herzhaftes Frühstück!",
+    "🌱 Tofu-Scramble ist eine vegane Alternative zu Rührei und reich an pflanzlichem Protein!"
 }
 
 # Knopf für eine zufällige Emoji-Überraschung zum Thema Frühstück
@@ -149,3 +204,7 @@ st.sidebar.markdown("""
 - **Probiotika:** Gut für die Darmgesundheit und die Verdauung. Produktempfehlung: [Burgerstein Biotics G](https://www.burgerstein.ch/de-DE/produkte/burgerstein-biotics-g)
 - **Magnesium:** Kann helfen, Muskelkrämpfe zu reduzieren und hilft bei Müdigkeit. Produktempfehlung: [Burgerstein Magnesiumvital Direct](https://www.burgerstein.ch/de-DE/produkte/burgerstein-magnesiumvital-direct)
 """)
+
+# Footer
+st.markdown("---")
+st.markdown("Erstellt von FoodisGood 🍳 Genieße deine Mahlzeiten und dein Studium! 😊")
